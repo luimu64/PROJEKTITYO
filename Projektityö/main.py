e5a752152d8ef88f75befe2f83d65010e6578c2c
leimu raittinen <54173370+luimu64@users.noreply.github.com>
2020-01-22 12:55:04 +0200

    Update main.py

--- Projektityö/main.py
+++ Projektityö/main.py
@@ -2,22 +2,24 @@ import time
 import sys
 import keyboard
 
-teksti1 = """Pimeänä ja kosteana tammikuun perjantai iltana olet menossa 
+t1 = """Pimeänä ja kosteana tammikuun perjantai iltana olet menossa 
 ostamaan kaljaa k-marketista rankan amis päivän jälkeen. Matkalla 
 kuitenkin virolainen raksamies ajaa Kännissä päältäsi saabilla. 
 Heräät aamulla joesta mutta tuntuu että jotain on pielessä. 
 Vesi on liian lämmintä ja on liian kirkasta ollakseen tammikuu
 saati sitten suomi ollenkaan. Huomaaat että sinulla on kova nälkä.\n"""
 
-#dp functio stackoverflowsta https://stackoverflow.com/questions/9246076/how-to-print-one-character-at-a-time-on-one-line
+t1_1 = """Teksti kaksi"""
+
+#https://stackoverflow.com/questions/9246076/how-to-print-one-character-at-a-time-on-one-line
 def animoija(s):
     for a in s:
         sys.stdout.write(a)
         sys.stdout.flush()
         time.sleep(0.07)
-        if keyboard.is_pressed('enter'):
+        if keyboard.is_pressed('space'):
             refreshi()
-            print(teksti1)
+            print(s)
             break
         
 
@@ -28,5 +30,12 @@ def refreshi():
         i += 1
 
 refreshi()
-animoija(teksti1)
+animoija(t1)
+input("""
+1.  Jää ojaan kalastamaan sinulla ei ole onkea mutta kyllähän 
+    karhutkin voivat kalaa napata käsillä\n
+2.  Lähde etsimään ruokaa luonnosta\n
+3.  Seuraa joen viereistä polkua toivoes löytäväsi siviilisaatiota\n
+
+""")
 time.sleep(5)
