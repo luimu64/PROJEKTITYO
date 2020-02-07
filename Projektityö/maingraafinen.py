0ec3d124e88cc19ab3d8f8da1b9edaa5919329f7
luimu64 <54173370+luimu64@users.noreply.github.com>
2020-02-07 13:12:15 +0200

    update

--- Projektityö/maingraafinen.py
+++ Projektityö/maingraafinen.py
@@ -1,23 +1,36 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 root = Tk()
 root.title("Isekai.py")
-root.geometry("1000x400")
-fraami = LabelFrame(root, text="Kontrollit", padx=50, pady=5)
-fraami.pack()
+ulkofraami = LabelFrame(root, pady=5)
+ulkofraami.grid(row=0,column=0,columnspan=3)
+nappifraami = LabelFrame(ulkofraami, padx=50, pady=5)
+nappifraami.grid(row=2,column=0,columnspan=3)
 
-kontrollit = "1, 2, 3...  =  vaihtoehtojen valitsemisnäppäimet jos ei toisin sanota\nvälilyönti pohjassa  =  skippaa tekstin loppuun"
+def update(path,tekstinro):
+    teksti.configure(text="tekstilista[tekstinro]")
 
-teksti = Label(fraami, text=kontrollit)
-teksti.pack()
-def pohjateksti(teksti):
-    teksti = Label(fraami, text=" Pimeänä ja kosteana tammikuun perjantai iltana olet menossa ostamaan kaljaa k-marketista rankan amispäivän jälkeen. Matkalla kuitenkin virolainen raksamies ajaa kännissä päältäsi saabilla. Heräät aamulla joesta mutta tuntuu että jotain on pielessä. Vesi on liian lämmintä ja on liian kirkasta ollakseen tammikuusaati sitten suomi ollenkaan. Huomaaat että sinulla on kova nälkä")
-    teksti.pack()
-    
+intro = "Pimeänä ja kosteana tammikuun perjantai iltana olet menossa \nostamaan kaljaa k-marketista rankan amispäivän jälkeen. Matkalla \nkuitenkin virolainen raksamies ajaa kännissä päältäsi saabilla. "
+kuva = ImageTk.PhotoImage(Image.open("kuvat/joki.jpg"))
+kuval = Label(ulkofraami, image=kuva)
+kuval.grid(row=0,column=0)
 
-strnappi = Button(root, text="Aloita!", padx=50, command=pohjateksti("s"))
-strnappi.pack()
+teksti = Label(ulkofraami, text=intro)
+teksti.grid(row=1, column=0)
+
+tekstilista[0] = intro
 
 
+seuraava = Button(nappifraami, text="Seuraava", padx=10)
+seuraava.grid(row=0, column=0)
+stats = Button(nappifraami, text="Tiedot", padx=10)
+stats.grid(row=0, column=1)
+valinta1 = Button(nappifraami, text="valinta 1", padx=10)
+valinta1.grid(row=0, column=2)
+valinta2 = Button(nappifraami, text="valinta 2", padx=10)
+valinta2.grid(row=0, column=3)
+valinta3 = Button(nappifraami, text="valinta 3", padx=10)
+valinta3.grid(row=0, column=4)
 
 root.mainloop()
